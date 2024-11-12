ea42dc375c61f0517ff184ae4930391c97444e2d
antonbaider <email.b.anton@gmail.com>
2024-11-13 00:54:53 +0100

    v5 https added

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { useAuthStore } from '@/stores/auth';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
+    baseURL: import.meta.env.VITE_API_URL || 'https://localhost:8443',
     headers: {
         'Content-Type': 'application/json',
     },
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import fs from 'fs';
+
+// Define paths for certificate files
+const certPath = process.env.VITE_SSL_CERT_PATH || '/Users/antonbaider/dev/mthree/mbank/front/nginx/ssl/cert.pem';
+const keyPath = process.env.VITE_SSL_KEY_PATH || '/Users/antonbaider/dev/mthree/mbank/front/nginx/ssl/private.key';
 
 export default defineConfig({
   plugins: [vue()],
@@ -9,5 +13,12 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'), // Alias "@" to "src" directory
     },
+  },
+  server: {
+    https: {
+      key: fs.readFileSync(keyPath),  // Path to your private key
+      cert: fs.readFileSync(certPath),     // Path to your certificate
+    },
+    port: 3000,  // You can change the port if needed
   },
 });
